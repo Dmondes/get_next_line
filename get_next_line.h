85910a48b251672fa110c0e96873b2436d7050cd
Desmond <106146898+Dmondes@users.noreply.github.com>
2024-02-23 17:23:36 +0800

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -2,8 +2,15 @@
 # define GET_NEXT_LINE_H
 
 # include <unistd.h>
+# include <fcntl.h>
+# include <stdio.h>
 # include <stdlib.h>
 
-char *get_next_line(int fd);
-
+void    *ft_calloc(int count, int size);
+int             ft_strlen(const char *s);
+char    *ft_strchr(char *s, int c);
+char    *ft_strdup(char *src);
+char    *ft_substr(char const *s, int start, int len);
+char    *get_next_line(int fd);
 #endif
+
