faa15c75a3705e17d9b8c6a344312bbb7bfa989f
Desmond <106146898+Dmondes@users.noreply.github.com>
2024-02-24 18:06:29 +0800

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: delim <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/13 10:06:38 by delim             #+#    #+#             */
+/*   Updated: 2024/02/24 17:45:47 by delim            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
@@ -6,11 +18,10 @@
 # include <stdio.h>
 # include <stdlib.h>
 
-void    *ft_calloc(int count, int size);
-int             ft_strlen(const char *s);
-char    *ft_strchr(char *s, int c);
-char    *ft_strdup(char *src);
-char    *ft_substr(char const *s, int start, int len);
-char    *get_next_line(int fd);
+int		ft_strlen(const char *s);
+char	*ft_strdup(char *src);
+char	*ft_strchr(char *s, int c);
+char	*ft_substr(char const *s, int start, int len);
+char	*get_next_line(int fd);
+void	*ft_memmove(void *dest, const void *src, int n);
 #endif
-
